60f076c97bc5e7a3333dc6c98b543a0e1e4a2412
Priyanshu Vora <92289465+privo211@users.noreply.github.com>
2025-02-18 12:26:10 -0500

    Correcting Header

--- resumex/src/app/register/page.tsx
+++ resumex/src/app/register/page.tsx
@@ -1,4 +1,5 @@
-import Header from "~/components/ui/Header";
+import Header from "../_components/Header";
+import Footer from "../_components/Footer";
 import Image from "next/image";
 import { RegisterForm } from "~/components/register-form";
 
