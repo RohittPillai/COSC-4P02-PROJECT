24333f10f8797e6b9d120a6bbb102d06e6ea62bb
Priyanshu Vora <92289465+privo211@users.noreply.github.com>
2025-02-22 19:21:55 -0500

    Logo Edits

--- resumex/src/app/register/page.tsx
+++ resumex/src/app/register/page.tsx
@@ -7,10 +7,10 @@ export default function RegisterPage() {
   return (
     <>
       <Header />
-      <div className="flex min-h-screen flex-col items-center justify-center bg-muted p-6 md:p-10">
-        <div className="flex w-full max-w-sm flex-col items-center self-center transform -translate-y-12">
+      <div className="flex min-h-screen flex-col items-center justify-center gap-8 bg-muted p-6 md:p-10">
+        <div className="flex w-full max-w-sm flex-col items-center gap-10 self-center">
           <Image
-            src="/main_logo.png"
+            src="/transparent_black_main_logo.png"
             alt="ResumeX"
             width={200}
             height={200}
