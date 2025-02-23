77a6218e126b19b34acb62335191f01eca46afef
Priyanshu Vora <92289465+privo211@users.noreply.github.com>
2025-02-23 18:56:59 -0500

    Update register/route.ts

--- resumex/src/app/api/auth/register/route.ts
+++ resumex/src/app/api/auth/register/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import prisma from '~/lib/prisma'; // Make sure your Prisma client is correctly configured
+import prisma from '~/lib/prisma';
 import bcrypt from 'bcryptjs';
 
 export async function POST(request: Request) {
