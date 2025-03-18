5691a8f31b9a2b058a8cdbc31beec53662eeaa9b
JennyD-11 <jennydobariya6014@gmail.com>
2025-03-18 14:52:16 -0400

    template 1 corrected

--- resumex/src/app/templates/template2/page.tsx
+++
@@ -1,5 +0,0 @@
-"use client";
-
-export default function Template2Page() {
-  return <h1 className="text-center text-3xl font-bold mt-10">Welcome to Template 2</h1>;
-}
--- resumex/src/app/templates/template3/page.tsx
+++
@@ -1,5 +0,0 @@
-"use client";
-
-export default function Template3Page() {
-  return <h1 className="text-center text-3xl font-bold mt-10">Welcome to Template 3</h1>;
-}
