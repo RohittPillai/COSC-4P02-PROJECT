3bf73d73d082147dcb145367e5cfefe465cdd230
NeetiPandya27 <np20ag@brocku.ca>
2025-04-23 04:13:27 -0400

    treied to fix the exposed token thing

--- resumex/src/app/resume-view/page.tsx
+++ resumex/src/app/resume-view/page.tsx
@@ -41,7 +41,6 @@ export default function ResumeViewPage() {
             }
             setLoading(false);
         };
-
         fetchResume();
     }, [resumeId]);
 
--- resumex/src/lib/supabaseClient.ts
+++ resumex/src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://daisltwqqluqqujyatgb.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRhaXNsdHdxcWx1cXF1anlhdGdiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUzODM4MzUsImV4cCI6MjA2MDk1OTgzNX0.Z4wuIeg9VzaU3cq0SmSx8k-KSioo0lM6kaC4_pwDDW4';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
