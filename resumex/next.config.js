82c83c7a206a0d3a04c32c06d375d76ca925812e
Priyanshu Vora <92289465+privo211@users.noreply.github.com>
2025-04-23 16:02:07 -0400

    Commented out "swcMinify: true" in next.config.js to avoid "unrecognized" errors

--- resumex/next.config.js
+++ resumex/next.config.js
@@ -6,7 +6,7 @@ import "./src/env.js";
 
 /** @type {import('next').NextConfig} */
 const config = {
-  swcMinify: true,
+  //swcMinify: true,
   compiler: {
     removeConsole: process.env.NODE_ENV === "production",
   },
