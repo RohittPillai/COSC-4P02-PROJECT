875c3a4e2ef22914bea2ef1f6aaaf2de907dfbac
Priyanshu Vora <92289465+privo211@users.noreply.github.com>
2025-04-24 13:53:55 -0400

    Changes for OAuth login in resumex.top

--- resumex/src/server/auth/config.ts
+++ resumex/src/server/auth/config.ts
@@ -39,7 +39,8 @@ export const authConfig = {
     }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      checks: ["pkce"],
     })
     /**
      * ...add more providers here.
