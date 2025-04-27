e512c57e5e3bb355ec4481bc65bbecf626f90a83
mahim66601 <mahiimm66601@gmail.com>
2025-04-27 17:26:19 -0400

    Discord Login bug fix by Priyanshu

--- resumex/src/env.js
+++ resumex/src/env.js
@@ -12,7 +12,11 @@ export const env = createEnv({
         ? z.string()
         : z.string().optional(),
     AUTH_DISCORD_ID: z.string(),
-    AUTH_DISCORD_SECRET: z.string(),
+    AUTH_DISCORD_PUBLIC_KEY:z.string(),
+    // Discord OAuth2 (for NextAuth)
+    DISCORD_CLIENT_ID:     z.string(),
+    DISCORD_CLIENT_SECRET: z.string(),
+
     DATABASE_URL: z.string().url(),
     NODE_ENV: z
       .enum(["development", "test", "production"])
@@ -35,7 +39,9 @@ export const env = createEnv({
   runtimeEnv: {
     AUTH_SECRET: process.env.AUTH_SECRET,
     AUTH_DISCORD_ID: process.env.AUTH_DISCORD_ID,
-    AUTH_DISCORD_SECRET: process.env.AUTH_DISCORD_SECRET,
+    AUTH_DISCORD_PUBLIC_KEY:  process.env.AUTH_DISCORD_PUBLIC_KEY,
+    DISCORD_CLIENT_ID:        process.env.DISCORD_CLIENT_ID,
+    DISCORD_CLIENT_SECRET:    process.env.DISCORD_CLIENT_SECRET,
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
   },
