073abcdcd1734187bd815ecedd3c1f04162738bf
mahim66601 <mahiimm66601@gmail.com>
2025-04-27 16:01:38 -0400

    Social login fix by Priyanshy

--- resumex/src/trpc/react.tsx
+++ resumex/src/trpc/react.tsx
@@ -69,9 +69,16 @@ export function TRPCReactProvider(props: { children: React.ReactNode }) {
   );
 }
 
+// function getBaseUrl() {
+//   if (typeof window !== "undefined") return window.location.origin;
+//   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+//   if (process.env.NEXTAUTH_URL) return process.env.NEXTAUTH_URL;
+//   return `http://localhost:${process.env.PORT ?? 3000}`;
+// }
 function getBaseUrl() {
-  if (typeof window !== "undefined") return window.location.origin;
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
-  if (process.env.NEXTAUTH_URL) return process.env.NEXTAUTH_URL;
-  return `http://localhost:${process.env.PORT ?? 3000}`;
+  if (typeof window !== "undefined") {
+    return "";
+  }
+
+  return process.env.NEXT_PUBLIC_BASE_URL!;
 }
